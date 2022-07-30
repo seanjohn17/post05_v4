d2906a3f63ccae104eb9094512cb5f1f9fce45e3
seanjohn17 <110308338+seanjohn17@users.noreply.github.com>
2022-07-30 17:10:41 -0500

    Update post05.go

--- post05.go
+++ post05.go
@@ -22,6 +22,7 @@ var (
 // Coursedata table + Coursename
 type MSDSCourse struct {
 	ID          int
+	Coursecode  string
 	CID         string `json:"course_ID"`
 	CNAME       string `json:"course_name"`
 	CPREREQ     string `json:"prerequisite"`
@@ -40,10 +41,10 @@ func openConnection() (*sql.DB, error) {
 	return db, nil
 }
 
-// The function returns the Course ID of the course
+// The function returns the Course ID of the coursecode
 // -1 if the course does not exist
-func exists(CID string) int {
-	CID = strings.ToLower(CID)
+func exists(coursecode string) int {
+	coursecode = strings.ToLower(coursecode)
 
 	db, err := openConnection()
 	if err != nil {
@@ -53,17 +54,17 @@ func exists(CID string) int {
 	defer db.Close()
 
 	courseID := -1
-	statement := fmt.Sprintf(`SELECT "ID" FROM "courses" where CID = '%s'`, CID)
+	statement := fmt.Sprintf(`SELECT "id" FROM "courses" where coursecode = '%s'`, coursecode)
 	rows, err := db.Query(statement)
 
 	for rows.Next() {
-		var ID int
-		err = rows.Scan(&ID)
+		var id int
+		err = rows.Scan(&id)
 		if err != nil {
 			fmt.Println("Scan", err)
 			return -1
 		}
-		courseID = ID
+		courseID = id
 	}
 	defer rows.Close()
 	return courseID
@@ -73,7 +74,7 @@ func exists(CID string) int {
 // Returns new Course ID
 // -1 if there was an error
 func AddCourse(d MSDSCourse) int {
-	d.CID = strings.ToLower(d.CID)
+	d.Coursecode = strings.ToLower(d.Coursecode)
 
 	db, err := openConnection()
 	if err != nil {
@@ -82,25 +83,25 @@ func AddCourse(d MSDSCourse) int {
 	}
 	defer db.Close()
 
-	courseID := exists(d.CID)
+	courseID := exists(d.Coursecode)
 	if courseID != -1 {
-		fmt.Println("Course already exists:", d.CID)
+		fmt.Println("Course already exists:", d.Coursecode)
 		return -1
 	}
 
-	insertStatement := `insert into "courses" ("CID") values ($1)`
-	_, err = db.Exec(insertStatement, d.CID)
+	insertStatement := `insert into "courses" ("coursecode") values ($1)`
+	_, err = db.Exec(insertStatement, d.Coursecode)
 	if err != nil {
 		fmt.Println(err)
 		return -1
 	}
 
-	courseID = exists(d.CID)
+	courseID = exists(d.Coursecode)
 	if courseID == -1 {
 		return courseID
 	}
 
-	insertStatement = `insert into "coursedata" ("ID", "CID", "CNAME", "CPREREQ")
+	insertStatement = `insert into "coursedata" ("id", "cid", "cname", "cprereq")
 	values ($1, $2, $3, $4)`
 	_, err = db.Exec(insertStatement, courseID, d.CID, d.CNAME, d.CPREREQ)
 	if err != nil {
@@ -120,24 +121,24 @@ func DeleteCourse(id int) error {
 	defer db.Close()
 
 	// Does the ID exist?
-	statement := fmt.Sprintf(`SELECT "CID" FROM "courses" where id = %d`, id)
+	statement := fmt.Sprintf(`SELECT "coursecode" FROM "courses" where id = %d`, id)
 	rows, err := db.Query(statement)
 
-	var CID string
+	var coursecode string
 	for rows.Next() {
-		err = rows.Scan(&CID)
+		err = rows.Scan(&coursecode)
 		if err != nil {
 			return err
 		}
 	}
 	defer rows.Close()
 
-	if exists(CID) != id {
+	if exists(coursecode) != id {
 		return fmt.Errorf("Course with ID %d does not exist", id)
 	}
 
 	// Delete from MSDSCourse
-	deleteStatement := `delete from "coursedata" where ID=$1`
+	deleteStatement := `delete from "coursedata" where courseid=$1`
 	_, err = db.Exec(deleteStatement, id)
 	if err != nil {
 		return err
@@ -162,20 +163,21 @@ func ListCourses() ([]MSDSCourse, error) {
 	}
 	defer db.Close()
 
-	rows, err := db.Query(`SELECT "ID","CID","CNAME","CPREREQ"
+	rows, err := db.Query(`SELECT "id","coursecode","cid","cname","cprereq"
 		FROM "courses","coursedata"
-		WHERE courses.CID = coursedata.CID`)
+		WHERE courses.id = coursedata.courseid`)
 	if err != nil {
 		return Data, err
 	}
 
 	for rows.Next() {
 		var id int
+		var coursecode string
 		var courseid string
 		var coursename string
 		var prerequisite string
-		err = rows.Scan(&id, &courseid, &coursename, &prerequisite)
-		temp := MSDSCourse{ID: id, CID: courseid, CNAME: coursename, CPREREQ: prerequisite}
+		err = rows.Scan(&id, &coursecode, &courseid, &coursename, &prerequisite)
+		temp := MSDSCourse{ID: id, Coursecode: coursecode, CID: courseid, CNAME: coursename, CPREREQ: prerequisite}
 		Data = append(Data, temp)
 		if err != nil {
 			return Data, err
